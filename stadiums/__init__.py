71555b409fbe2d27dd7ed0287784f6a97c55b420
z33k <piotr.mazik@protonmail.com>
2024-04-14 20:27:58 +0200

    Scrape details and calculate cap2pop for towns

--- stadiums/__init__.py
+++ stadiums/__init__.py
@@ -7,11 +7,14 @@
     @author: z33k
 
 """
-from dataclasses import dataclass
+import random
+import re
+from collections import defaultdict
+from dataclasses import dataclass, asdict
 from datetime import datetime
 
-from stadiums.utils import extract_int, init_log
-from stadiums.utils.scrape import getsoup
+from stadiums.utils import extract_int, from_iterable, init_log
+from stadiums.utils.scrape import getsoup, throttled
 
 init_log()
 URL = "http://stadiony.net/stadiony/pol"
@@ -35,13 +38,17 @@ def _scrape_towns() -> list[Town]:
         try:
             name, county, voivod, area, pop, *_ = [
                 tag.text.strip() for tag in tr_tag.find_all("td")]
-            towns.append(Town(name, county, voivod, int(area), int(pop)))
+            towns.append(Town(name, county.replace("[a]", ""), voivod, int(area), int(pop)))
         except ValueError:
             pass
     return towns
 
 
 TOWNS = _scrape_towns()
+TOWNS.extend([
+    Town(name="Nieciecza", county="tarnowski", voivodeship="małopolskie", population=682, area=490),
+    Town(name="Stężyca", county="kartuski", voivodeship="pomorskie", population=2165, area=0),
+])
 
 
 @dataclass(frozen=True)
@@ -49,8 +56,12 @@ class _BasicStadium:
     name: str
     url: str
     town: str
-    clubs: tuple[str]
+    clubs: tuple[str, ...]
     capacity: int
+
+    @property
+    def as_dict(self) -> dict[str, str | tuple[str, ...] | int]:
+        return {k: v for k, v in asdict(self).items() if k != "url"}
 
 
 def _scrape_basic_data() -> list[_BasicStadium]:
@@ -71,6 +82,7 @@ def _scrape_basic_data() -> list[_BasicStadium]:
 
 
 BASIC_STADIUMS = _scrape_basic_data()
+_KORONA_INAUGURATION = datetime(2006, 4, 1, 0, 0)
 
 
 @dataclass(frozen=True)
@@ -78,13 +90,103 @@ class Stadium:
     name: str
     town: str
     country: str
-    clubs: tuple[str]
+    clubs: tuple[str, ...]
     capacity: int
     inauguration: datetime
-    renovations: list[datetime]
+    renovation: datetime | None
     cost: int | None  # in PLN
     illumination: int | None  # in lux
 
+    @property
+    def is_modern(self) -> bool:
+        if self.cost is None:
+            return False
+        dates = [d for d in (self.inauguration, self.renovation) if d is not None]
+        if not dates:
+            return False
+        date = max(dates)
+        return date >= _KORONA_INAUGURATION
 
 
+_DATE_REGEX = re.compile(r"\b(?:\d{2}\.\d{2}\.\d{4}|\d{4})(?:\s*\w+)*\b")
+_DT_FMT = "%d.%m.%Y"
+
+
+def throttling_delay() -> float:
+    return round(random.uniform(0.4, 0.6), 3)
+
+
+@throttled(throttling_delay)
+def _scrape_details(basic_data: _BasicStadium) -> Stadium:
+    soup = getsoup(basic_data.url)
+    table = soup.find("table", class_="stadium-info")
+    country, inauguration, renovation, cost, illumination = None, None, None, None, None
+    for row in table.find_all("tr"):
+        match row.find("th").text:
+            case "Kraj":
+                country = row.find("td").find("a").text.strip().strip('"')
+            case "Inauguracja":
+                text = row.find("td").text.strip()
+                date = _DATE_REGEX.search(text)
+                if date:
+                    date = date.group()
+                    inauguration = datetime.strptime(date, _DT_FMT) if len(
+                        date) == 10 else datetime(int(date), 1, 1)
+            case "Renowacje":
+                years = []
+                for token in row.find("td").text.strip().split(", "):
+                    if "-" in token:
+                        years.extend(token.split("-"))
+                    elif "–" in token:
+                        years.extend(token.split("–"))
+                    else:
+                        years.append(token)
+                renovation = max(int(year) for year in years)
+                renovation = datetime(renovation, 1, 1)
+            case "Koszt":
+                text = row.find("td").text.strip()
+                cost_int = extract_int(text)
+                cost = cost_int * 1_000_000_000 if "mld" in text else cost_int * 1_000_000
+            case "Oświetlenie":
+                illumination = extract_int(row.find("td").text.strip())
+            case _:
+                pass
+
+    return Stadium(
+        **basic_data.as_dict,
+        country=country,
+        inauguration=inauguration,
+        renovation=renovation,
+        cost=cost,
+        illumination=illumination
+    )
+
+
+STADIUMS = [_scrape_details(bs) for bs in BASIC_STADIUMS]
+MODERN_STADIUMS = [s for s in STADIUMS if s.is_modern]
+
+
+def _stadiums_per_town() -> list:
+    towns = {t.name: t for t in TOWNS}
+    aggregated = defaultdict(list)
+    for stadium in MODERN_STADIUMS:
+        aggregated[stadium.town].append(stadium)
+
+    result = []
+    for town, stadiums in aggregated.items():
+        pop = towns[town].population
+        cap = sum(s.capacity for s in stadiums)
+        result.append(
+            {
+                "town": town,
+                "stadiums": [asdict(s) for s in stadiums],
+                "population": pop,
+                "total_capacity": cap,
+                "cap2pop": f"{cap / pop * 100:.2f} %",
+            }
+        )
+    return result
+
+
+STADIUMS_PER_TOWN = _stadiums_per_town()
 
