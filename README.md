65b9bd7b0d4dd8ea43e7718a9442265cd6c81862
z33k <piotr.mazik@protonmail.com>
2024-04-14 16:03:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# stadiums
+Scrape Polish stadiums data
+
+### Usage
+This is a hobby project with no other intents so far
---
+++ stadiums/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+    stadiums.__init__.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Scrape data Polish stadiums.
+
+    @author: z33k
+
+"""
