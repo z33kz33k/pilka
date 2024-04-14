feba4ba0e958a91e368b90cdb36cb48cca2cdf68
z33k <piotr.mazik@protonmail.com>
2024-04-14 18:30:12 +0200

    Scrape basic stadium data

--- stadiums/__init__.py
+++ stadiums/__init__.py
@@ -10,7 +10,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-from stadiums.utils import init_log
+from stadiums.utils import extract_int, init_log
 from stadiums.utils.scrape import getsoup
 
 init_log()
@@ -33,7 +33,8 @@ def _scrape_towns() -> list[Town]:
     towns = []
     for tr_tag in table.select("tbody tr"):
         try:
-            name, county, voivod, area, pop, *_ = [tag.text for tag in tr_tag.find_all("td")]
+            name, county, voivod, area, pop, *_ = [
+                tag.text.strip() for tag in tr_tag.find_all("td")]
             towns.append(Town(name, county, voivod, int(area), int(pop)))
         except ValueError:
             pass
@@ -44,6 +45,35 @@ TOWNS = _scrape_towns()
 
 
 @dataclass(frozen=True)
+class _BasicStadium:
+    name: str
+    url: str
+    town: str
+    clubs: tuple[str]
+    capacity: int
+
+
+def _scrape_basic_data() -> list[_BasicStadium]:
+    soup = getsoup(URL)
+    tables = soup.find_all("table", limit=4)
+    stadiums = []
+    for table in tables:
+        for row in table.find_all("tr")[1:]:
+            name_tag, town_tag, clubs_tag, cap_tag = row.find_all("td")
+            name, url = name_tag.text.strip(), name_tag.find("a").attrs["href"]
+            town = town_tag.text.strip()
+            clubs = [club.strip() for club in clubs_tag.text.split(", ") if club.strip() != "-"]
+            # trim redundant town info in clubs
+            clubs = [club.replace(f" {town}", "") for club in clubs]
+            cap = extract_int(cap_tag.text)
+            stadiums.append(_BasicStadium(name, url, town, tuple(clubs), cap))
+    return stadiums
+
+
+BASIC_STADIUMS = _scrape_basic_data()
+
+
+@dataclass(frozen=True)
 class Stadium:
     name: str
     town: str
@@ -51,7 +81,10 @@ class Stadium:
     clubs: tuple[str]
     capacity: int
     inauguration: datetime
-    cost: int | None
+    renovations: list[datetime]
+    cost: int | None  # in PLN
     illumination: int | None  # in lux
+
+
 
 
